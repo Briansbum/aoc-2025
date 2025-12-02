7c04aaceca3100a76b192c1e2d6c9bb858a02708
Alex Freestone <alex.freestone@doccla.com>
2025-12-02 10:08:46 +0000

    Switch to kebab case

--- days/day1.clj
+++ days/day1.clj
@@ -9,20 +9,20 @@
 (ns day1)
 
 (def acc 0)
-(def initial_arrow 50)
+(def initial-arrow 50)
 (def options 100)
 
-(defn split_instruction [ins]
+(defn split-instruction [ins]
   (list (subs ins 0 1) (subs ins 1)))
 
 (defn instructions [f]
   (with-open [rdr (clojure.java.io/reader f)]
   (reduce conj [] (line-seq rdr))))
 
-(defn instructions_splits [xs]
-  (map split_instruction xs))
+(defn instructions-splits [xs]
+  (map split-instruction xs))
 
-(defn move_arrow [arrow count_loops direction jump]
+(defn move-arrow [arrow count-loops direction jump]
   (def j (Integer/parseInt jump))
 
   ; this is the number of times the thing loops
@@ -31,48 +31,48 @@
   (def i (if (> j options) 
     (- j (* loops options)) 
     j))
-  (def clicks_from_loops (if (true? count_loops) loops 0))
+  (def clicks-from-loops (if (true? count-loops) loops 0))
 
   ; do not count a click if we are starting on 0
-  (def started_at_zero (= arrow 0))
+  (def started-at-zero (= arrow 0))
 
   (if (= "L" direction) 
   (if (> i arrow) 
-    ; underflow - only count wrap if count_loops AND not starting at 0
-    (let [new_pos (- options (- i arrow))]
-      (list new_pos (+ clicks_from_loops (if (and count_loops (not started_at_zero)) 1 0))))
+    ; underflow - only count wrap if count-loops AND not starting at 0
+    (let [new-pos (- options (- i arrow))]
+      (list new-pos (+ clicks-from-loops (if (and count-loops (not started-at-zero)) 1 0))))
     ; move left
-    (list (- arrow i) clicks_from_loops)) 
+    (list (- arrow i) clicks-from-loops)) 
   (if (> i (- (- options 1) arrow))
-    ; overflow - only count wrap if count_loops AND we don't land on 0
-    (let [new_pos (- (- i (- (- options 1) arrow)) 1)]
-      (list new_pos (+ clicks_from_loops (if (and count_loops (not= new_pos 0)) 1 0))))
+    ; overflow - only count wrap if count-loops AND we don't land on 0
+    (let [new-pos (- (- i (- (- options 1) arrow)) 1)]
+      (list new-pos (+ clicks-from-loops (if (and count-loops (not= new-pos 0)) 1 0))))
     ; move right
-    (list (+ arrow i) clicks_from_loops)))
+    (list (+ arrow i) clicks-from-loops)))
 )
 
-(defn move_and_read [info ins]
-  ; arrow_pos, loop_count_state, direction, jump
-  (def move_arrow_res (move_arrow (first info) (nth info 2) (first ins) (second ins)))
-  (if (= (first move_arrow_res) 0)
-    ; new_arrow_pos, acc, loop_count_state
-    (list (first move_arrow_res) (+ (second info) (second move_arrow_res) 1) (nth info 2))
-    (list (first move_arrow_res) (+ (second info) (second move_arrow_res)) (nth info 2))
+(defn move-and-read [info ins]
+  ; arrow-pos, loop-count-state, direction, jump
+  (def move-arrow-res (move-arrow (first info) (nth info 2) (first ins) (second ins)))
+  (if (= (first move-arrow-res) 0)
+    ; new-arrow-pos, acc, loop-count-state
+    (list (first move-arrow-res) (+ (second info) (second move-arrow-res) 1) (nth info 2))
+    (list (first move-arrow-res) (+ (second info) (second move-arrow-res)) (nth info 2))
   )
 )
 
-(def out1 (reduce move_and_read (list initial_arrow 0 false) (instructions_splits (instructions "fixtures/day1-p1.simple"))))
+(def out1 (reduce move-and-read (list initial-arrow 0 false) (instructions-splits (instructions "fixtures/day1-p1.simple"))))
 
 (println ["Day 1 Part 1 Simple" (second out1)])
 
-(def out2 (reduce move_and_read (list initial_arrow 0 false) (instructions_splits (instructions "fixtures/day1-p1.full"))))
+(def out2 (reduce move-and-read (list initial-arrow 0 false) (instructions-splits (instructions "fixtures/day1-p1.full"))))
 
 (println ["Day 1 Part 1 Full" (second out2)])
 
-(def out3 (reduce move_and_read (list initial_arrow 0 true) (instructions_splits (instructions "fixtures/day1-p1.simple"))))
+(def out3 (reduce move-and-read (list initial-arrow 0 true) (instructions-splits (instructions "fixtures/day1-p1.simple"))))
 
 (println ["Day 1 Part 2 Simple" (second out3)])
 
-(def out4 (reduce move_and_read (list initial_arrow 0 true) (instructions_splits (instructions "fixtures/day1-p1.full"))))
+(def out4 (reduce move-and-read (list initial-arrow 0 true) (instructions-splits (instructions "fixtures/day1-p1.full"))))
 
 (println ["Day 1 Part 2 Full" (second out4)])
