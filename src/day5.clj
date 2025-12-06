a8c2dafb661b4138b3b2601337e1c741f2500b98
Alex Freestone <alex.freestone@doccla.com>
2025-12-06 20:52:22 +0000

    start day 6

--- src/day5.clj
+++ src/day5.clj
@@ -1,7 +1,6 @@
-(ns day4
+(ns day5
   (:require [clojure.edn :as edn])
   (:require [clojure.string :as s])
-  (:require [clojure.set :as st])
   (:require [file-ops :as fops])
   (:require [string-ops :as sops])
   (:require [clojure.java.io :as io]))
---
+++ src/day6.clj
@@ -0,0 +1,6 @@
+(ns day5
+  (:require [clojure.edn :as edn])
+  (:require [clojure.string :as s])
+  (:require [file-ops :as fops])
+  (:require [string-ops :as sops])
+  (:require [clojure.java.io :as io]))
