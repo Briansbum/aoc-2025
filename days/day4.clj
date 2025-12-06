f81afd3a323ec062ce532f24f62db61d57c9332d
Alex Freestone <alex.freestone@doccla.com>
2025-12-06 10:36:19 +0000

    Finish day 4 part 1

--- days/day4.clj
+++ days/day4.clj
@@ -7,14 +7,6 @@
   (with-open [rdr (io/reader f)]
   (reduce conj [] (line-seq rdr))))
 
-(comment
-  It's the old 'you're looking at one line and need to check the others'
-  what we need to do is to operate on the entire 2 collection at once
-  we maintain a refernce to x and y and increment them at the same time
-  okay actually we only need to get the line length and then we can flatten
-  and move across the line in one direction with multiplication and division
-)
-
 (defn split-on-space [search]
   (s/split search #"\s"))
 
@@ -22,49 +14,67 @@
   (with-open [rdr (io/reader f)]
   (reduce conj [] (line-seq rdr))))
 
-(defn search [[fs acc] index]
+(defn search [[fs line-len acc] index]
   (let [
     result (cond
-            (< index 0) [fs acc]                  ; index is less than zero
-            (> index (- (count fs) 1)) [fs acc]    ; index is greater than fs len - 1
-            (= (nth fs index) \@) [fs (+ acc 1)] ; fs at index is @, 
-            :else [fs acc])                         ; fs is probably at .
+            (< index 0) [fs line-len acc]                 ; index is less than zero
+            (> index (- (count fs) 1)) [fs line-len acc]  ; index is greater than fs len - 1
+            (= (nth fs index) \@) [fs line-len (+ acc 1)] ; fs at index is @, 
+            :else [fs line-len acc])                      ; fs is probably at .
             ]
-    (println (second result))
     result
   ))
-
-(def neighbor-deltas [[-1 -1] [-1 0] [-1 1]
-                      [0 -1]         [0 1]
-                      [1 -1]  [1 0]  [1 1]])
 
 (defn solve [[fs line-len acc] index]
   ; we now have the index so we need to just count the number 
   ; of cells with @'s in the surrounding 8 cells
   ; for each index we need to calculate first the grid location and then the 
   ; 8 surrounding cells. We can bin off any that are not in bounds
-   (println line-len acc index (nth fs index))
+  ; (println line-len acc index (nth fs index))
   (if (= (nth fs index) \@)
-  (let [positions [
-                  (- index (+ line-len 1))
-                  (- index line-len)
-                  (- index (- line-len 1))
-                  (+ index 1)
-                  (+ index (+ line-len 1))
-                  (+ index line-len)
-                  (+ index (- line-len 1))
-                  (- index 1)
-                   ]]
-    [fs line-len (+ acc 
-                    (if (> 4 (
-                              second (
-                                      reduce search [fs 0] positions))
-                              ) 1 0))])
-    [fs line-len acc]))
+    (let [
+          positions [
+                    (- index (+ line-len 1)) ; top left
+                    (- index line-len)       ; top
+                    (- index (- line-len 1)) ; top right
+                    (+ index 1)              ; right
+                    (+ index (+ line-len 1)) ; bottom right
+                    (+ index line-len)       ; bottom
+                    (+ index (- line-len 1)) ; bottom left
+                    (- index 1)              ; left
+                     ]
+          col (mod index line-len)
+          filtered-positions (cond
+                                 (= col 0) [
+                                            (nth positions 1)
+                                            (nth positions 2)
+                                            (nth positions 3)
+                                            (nth positions 4)
+                                            (nth positions 5)] ; no left positions
+                                 (= col (dec line-len)) [
+                                            (nth positions 0)
+                                            (nth positions 1)
+                                            (nth positions 5)
+                                            (nth positions 6)
+                                            (nth positions 7)] ; no right positions
+                                 :else positions ; we're in the middle of the grid
+                                )]
+      [fs line-len (+ acc 
+                      (let [
+                            result (reduce search [fs line-len 0] filtered-positions)
+                           ]
+                              (cond
+                                (> 4 (nth result 2)) 1
+                                :else 0)))])
+      [fs line-len acc]))
 
-(let [
-      instructions (read-instructions "fixtures/day4.simple")
-      line-len (count (first instructions))
-      flatstructions (mapcat seq instructions)
-      result (reduce solve [flatstructions line-len 0] (range (count flatstructions)))] 
-  (println (nth result 2)))
+(defn day4-part1 [f]
+  (let [
+        instructions (read-instructions f)
+        line-len (count (first instructions))
+        flatstructions (vec (mapcat seq instructions))
+        result (reduce solve [flatstructions line-len 0] (range (count flatstructions)))] 
+    (nth result 2)))
+
+(println (= 13 (day4-part1 "fixtures/day4.simple")))
+(println (day4-part1 "fixtures/day4.full"))
