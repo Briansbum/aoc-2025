cb31f67aa18b457302b04f11a373dde3c1dfac1f
Alex Freestone <alex.freestone@doccla.com>
2025-12-21 13:02:19 +0000

    I DID THE SAME THING AGAIN TWO WEEKS LATER

--- src/day7.clj
+++ src/day7.clj
@@ -3,79 +3,54 @@
   (:require [clojure.string :as s])
   (:require [file-ops :as fops])
   (:require [string-ops :as sops])
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.set :as cset]))
 
-(comment
-        prev-update-positions current-update-positions acc := [] [] 0
-        for i, l := range lines
--          if i == 0; 
--            prev-update-postitions (index-of l "S")
--            continue
+(defn fetch-indices [[acc line prev-index] index] 
+  (let[
+       i (= (nth line index) \^)
+      ]
+    (cond
+      (false? i) [acc line index]
+      :else [(conj acc index) line index])))
 
-          for j, c := range l
-            if (contains prev-update-positions j)
-              if (= c \^)
-                current-update-positions = append (dec j) (inc j)
-                l[(dec j)] l[(inc j)] = "|" "|"
-                (+ acc 2)
-              else
-                current-update-positions = append j
-                l[j] = "|"
+(defn one-more-one-less [acc i] (cset/union acc [(dec i) (inc i)]))
 
-          previous-update-positions = (dedupe current-update-positions)
-)
-
-(defn position-printer [positions line]
+(defn check-line [pipe-locs line]
   (let [
-;        f (println positions line)
-        cs (for [l1 line l2 (range (count line)) ])
-;        f (println cs)
-       ]()))
+        f (println "------------------new line----------------------")
+        f (println "pipe-locs: " pipe-locs)
+        f (println "line:      " line)
+        ; caret-locs contains the indicies of each ^ in this line as a set
+        caret-locs (set (first (reduce fetch-indices [[] line 0] (range (count line)))))
+        f (println "caret-locs: " caret-locs)
+        ; next each caret needs to generate two pipes into new-pipes-locs
+        new-pipes-locs (set (reduce one-more-one-less pipe-locs caret-locs))
+        f (println "new-pipes-locs:" new-pipes-locs)
+        ; new-pipes-locs needs to be turned into a set
 
-(defn character-checker [[acc curr-update-positions prev-update-positions line] i]
-  (let [
-        is-i-in-prev-update-positions ((complement nil?) (some #(= i %) prev-update-positions))
-;        f (println prev-update-positions i is-i-in-prev-update-positions)
+        ; pipe-locs (also a set) needs to difference() with caret-locs, call it pl-diff
+        pl-diff (cset/difference pipe-locs caret-locs)
+        npl-diff (cset/difference new-pipes-locs caret-locs)
 
-;        f(println curr-update-positions (dec i) i (inc i) (nth line i))
-        is-char-splitter (= \^ (nth line i))
-       ] (cond
-           (and is-i-in-prev-update-positions is-char-splitter)
-             [(+ acc 2) 
-              (conj curr-update-positions (dec i) (inc i))
-              prev-update-positions 
-              line]
-;           is-i-in-prev-update-positions
-;             [acc (conj curr-update-positions i) prev-update-positions line]
-           :else [acc curr-update-positions prev-update-positions line])))
+        f (println "pl-diff: " pl-diff)
+        ; then (union new-pipes-locs pl-diff) should be the return
+        f (println "result-union: " (cset/union npl-diff pl-diff))
+        
+       ] (cset/union npl-diff pl-diff)))
 
-(defn the-counter [[acc curr-update-positions prev-update-positions lines] line-index]
-  (let [
-        line (nth lines line-index)
-        character-check-results (reduce character-checker [0 curr-update-positions prev-update-positions line] (range (count line)))
-        curr-update-positions (set (second character-check-results))
-        f (println (count curr-update-positions) line-index acc  (second character-check-results))
-       ] 
-       [(+ acc (count curr-update-positions)) [] curr-update-positions lines]))
-
-(defn solve [f] 
+(defn solve [f]
   (let [
         lines (fops/read-instructions f)
 
-        ; this trickery is because escaping early from a reduce is annoying
-        ; luckily we have all of the input data so we know that line 1 is
-        ; always just 'S' at some index and we can use that to set initial
-        ; state while making the overall job easier
+        ; get the first index to set state and start looping from (rest lines)
         s-index (s/index-of (first lines) \S)
+        f (println (first lines) s-index)
 
-        ; after dithcing the first line we can also skip every other line
-        ; it doesn't do anything, at least for part 1
-        line-range (range 2 (- (count lines) 1) 2)
-        
-        counter (reduce the-counter [0 [] (set (list s-index)) lines] line-range)
-        
-       ]
-    counter))
+        ; 
+        result (count (reduce check-line (set [s-index]) (rest lines)))
+       ] 
+    result))
 
-(println (solve "fixtures/day7.simple"))
-(println (= 21 (solve "fixtures/day7.simple")))
+(println "result: " (solve "fixtures/day7.simple"))
+;(println (= 21 (solve "fixtures/day7.simple")))
